550eb45167c61dd52433e51c785ffdc3a59b4d9a
mafiapoii <bozenaef1@gmail.com>
2024-03-08 08:26:25 -0500

    first try for tea

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# tea-graphql
+# 🚀 fast-graphql
+
+Graphql Tools to Strucutre , Combine Resolvers , Merge Schema Definitions and generate schema file for Node.js / Next.Js / Graphql Apollo server
+
+Topics
+
+
+## Install
+
+```
+npm i fast-graphql
+```
+
+```
+yarn add fast-graphql
+```
+
+## Example : Next.js Graphql Server that use fast-graphql
+- [Nex.js Advanced Graphql Crud MongoDB Starter](https://github.com/idurar/starter-advanced-graphql-crud-next-js-mongodb) - Starter Generic CRUD with Advanced Apollo Graphql server with Next.js and Mongodb (TypeScript)
+
+- [https://github.com/idurar/next-js-graphql-apollo-server-starter](https://github.com/idurar/next-js-graphql-apollo-server-starter)
+
+
+## To Merge Schema
+
+```
+import { mergeSchemas } from 'fast-graphql';
+
+const pathfiles = './backend/graphql/typeDefs/*.gql';
+
+const mergedSchemas = mergeSchemas({ pathfiles });
+
+export default mergedSchemas;
+
+```
+
+## To Combine Resolvers
+
+```
+import { combineResolvers, resolverType } from 'fast-graphql';
+
+import user from './user';
+import place from './place';
+import review from './review';
+
+const resolvers: resolverType[] = [place, user, review];
+
+const cominedResolvers = combineResolvers({ resolvers });
+
+export default cominedResolvers;
+
+```
+
+## To Generate Schema file (recomended to deploy to vercel easly)
+
+```
+./scripts/generateSchema.js 
+
+const { generateSchema } = require('fast-graphql');
+
+const inputPath = './graphql/typeDefs/*.gql';
+const schemaPath = './graphql/schema.graphql';
+const typeDefsPath = './graphql/typeDefs/index.ts';
+
+generateSchema({ inputPath, schemaPath, typeDefsPath });
+
+```
+
+```
+# package.json
+
+"scripts": {
+    "dev": "yarn generate && next dev",
+    ...
+    "generate:schema": "node ./scripts/generateSchema"
+  },
+
+```
+
---
+++ index.js
@@ -0,0 +1,72 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+exports.__esModule = true;
+exports.generateSchema = exports.mergeSchemas = exports.combineResolvers = void 0;
+var path = require("path");
+var fs = require("fs");
+var glob = require("fast-glob");
+var combineResolvers = function (_a) {
+    var resolvers = _a.resolvers;
+    var Query = {};
+    var Mutation = {};
+    resolvers.map(function (resolver) {
+        if (resolver.hasOwnProperty("Query")) {
+            Query = __assign(__assign({}, Query), resolver.Query);
+        }
+        if (resolver.hasOwnProperty("Mutation")) {
+            Mutation = __assign(__assign({}, Mutation), resolver.Mutation);
+        }
+    });
+    return { Query: Query, Mutation: Mutation };
+};
+exports.combineResolvers = combineResolvers;
+var mergeSchemas = function (_a) {
+    var pathfiles = _a.pathfiles;
+    var schemas = [];
+    glob.sync(pathfiles).forEach(function (file) {
+        try {
+            var data = fs.readFileSync(path.resolve(file), "utf8");
+            schemas.push(data);
+        }
+        catch (err) {
+            throw new Error("Oops Error ! Couldn't merge definitions Type , check schemas");
+        }
+    });
+    return schemas;
+};
+exports.mergeSchemas = mergeSchemas;
+var generateSchema = function (_a) {
+    var inputPath = _a.inputPath, _b = _a.schemaPath, schemaPath = _b === void 0 ? "" : _b, _c = _a.typeDefsPath, typeDefsPath = _c === void 0 ? "" : _c;
+    var mergedSchemas = (0, exports.mergeSchemas)({ pathfiles: inputPath });
+    var schema = mergedSchemas.join(" ");
+    if (schemaPath !== "") {
+        fs.writeFile(schemaPath, schema, function (err) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });
+        console.log("  ✔ Schema Generated : ", schemaPath);
+    }
+    if (typeDefsPath !== "") {
+        var typeDefs = "export default `" + schema + "`;";
+        fs.writeFile(typeDefsPath, typeDefs, function (err) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });
+        console.log("  ✔ typeDefs Generated : ", typeDefsPath);
+    }
+};
+exports.generateSchema = generateSchema;
